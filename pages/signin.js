3c3266a676422a0f487f02c1f4c7e8edffd379ec
Edward P. Legaspi <czetsuya@gmail.com>
2021-11-19 13:22:00 +0800

    chore: add signin page

---
+++ pages/signin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import withSecurity from "../utils/withSecurity";
+
+const SigninPage = () => {
+
+  return (
+      <div>Hello</div>
+  )
+}
+
+export default withSecurity(SigninPage);
